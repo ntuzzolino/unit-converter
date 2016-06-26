203aff5418d334b24328bb983864446c8ba43a1e
nik tuzzolino <niktuzzolino@gmail.com>
2016-06-26 14:44:44 -0600

    Transfer and ReadMe documentaion

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # unit-converter
+One of my first challenges at BCW, the goal was to create a unit conversion Web application that could convert U.S. standard measurements to metric and vis-versa.
+
+I believe the skills we were supposed to walk away with from this project was writing pure code and using a correct order of operations with parens.
+
+Learning how to set an ActiveView value when using Angularn ng-show was a pretty cool way to update the view even though I hadn't learned how to use $stateProvider and component-based angular.
+
+The level of complextity in each controller would increase further down the conversions that i had. And although most people struggled with the very last question, I found the body mass index calculator to be more challenging than the very last question that happened to be the loan calculator. 
+
+
---
+++ controller.js
@@ -0,0 +1,80 @@
+app.controller('CounterController', function(){
+
+})
+app.controller('TempCon', function(){
+var tc = this;
+tc.temp = 0;
+
+tc.fToC = function(){
+    tc.temp = (tc.temp - 32) / 1.8;
+}
+tc.cToF = function(){
+    tc.temp = (tc.temp + 32) * 1.8;
+}
+
+})   
+    
+
+app.controller('LargeDist', function(){
+var ld = this;
+ld.count= 0;
+ld.mToK = function(){
+ld.count = (ld.count * 0.6214);
+
+   
+}
+ld.kToM = function(){
+    ld.count= (ld.count / 0.6214);
+}
+
+})
+
+app.controller('WeightCon', function(){
+    
+var wc = this;
+wc.count= 0;
+wc.lbToKg = function(){
+    wc.count = (wc.count * 2.2046);
+   
+}
+wc.kgToLb = function(){
+    wc.count = (wc.count / 2.2046);
+}
+
+}) 
+app.controller('InchCon', function(){
+var ic = this;
+ic.length= 0;
+ic.fToI = function(){
+    ic.length= (ic.length * 12);
+   
+}
+ic.iToF = function(){
+    ic.length= (ic.length / 12);
+}
+
+}) 
+app.controller('BodyCon', function(){
+var bc = this;
+bc.count= 0;
+bc.lbs= 0;
+bc.height= 0;
+bc.conToMI = function(){
+    bc.count= (bc.lbs / bc.height ^ 2) * 703;
+ //(Weight in Pounds / (Height in inches^2 ) ) x 703  
+}
+bc.conMiToDim = function(){
+    bc.count = (bc.lbs * (bc.height)) / 703;
+}
+
+}) 
+app.controller('LoanCalc', function(){
+var lcal = this;
+lcal.count= 0;
+
+lcal.estimate = function(){
+    lcal.count = lcal.amt * Math.pow(1 + ( lcal.rate / 100), lcal.years) ; 
+}
+
+})
+
